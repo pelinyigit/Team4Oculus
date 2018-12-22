bf270c3dad8954c3116ab404458e1c3059879e93
Ata <laythese@gmail.com>
2018-12-22 14:30:17 +0300

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package oculus;
 
 import java.sql.Connection;
@@ -12,30 +7,20 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-/**
- *
- * @author oculus_team_4
- */
+
 
 public class DBConnection 
 {
     
-    private static final String DRIVER = "org.apache.derby.jdbc.EmbeddedDriver"; //loading jdbc driver for apache server
-     private static final String JDBC_URL = "jdbc:derby:oculus;create=true";  //loading server's data location 
+    private static final String DRIVER ="org.apache.derby.jdbc.EmbeddedDriver"; //loading jdbc driver for apache server
+    private static final String JDBC_URL ="jdbc:derby:oculus;create=true;";  //loading server's data location 
     
+     Connection conn; 
      
-     Connection conn;     
-     
-    /**
-     */
     public DBConnection()
-           {
-        try {
+           {      
+        try {           
             this.conn = DriverManager.getConnection(JDBC_URL);
-            if(this.conn != null)
-            {
-                System.out.println("Connected to DB");
-            }
         } catch (SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -44,7 +29,8 @@ public class DBConnection
      public void createTable()
 {
         try {
-            conn.createStatement().execute("CREATE TABLE Mytable( Name varchar(50)");
+            conn.createStatement().execute("CREATE TABLE OCULUS\n" +
+"     (ID INT NOT NULL, NAME VARCHAR(50), PRIMARY KEY (ID)");
         } catch (SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -52,12 +38,13 @@ public class DBConnection
 
     /**
      *
-     * 
+     * @param ID
+     * @param NAME
      */
-    public void insertIntoTable(String name)
+    public void insertIntoTable(int ID,String NAME)
 {
         try {      
-            conn.createStatement().execute("INSERT INTO Mytable Values ("+name+")");
+            conn.createStatement().execute("INSERT INTO OCULUS VALUES('"+NAME+"',"+ID+")");
         } catch (SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -67,18 +54,16 @@ public void Display()
 {
         try {
             Statement statement = this.conn.createStatement();
-            ResultSet RS = statement.executeQuery("Select * FROM Mytable");
+            ResultSet res;
+            res = statement.executeQuery("SELECT * FROM OCULUS");
             
-            while(RS.next())
+            while(res.next())
             {
-                System.out.println(RS.getString("Name"));
+                System.out.println(res.getString("ID")+ " "+res.getString("NAME"));
             }
            
         } catch (SQLException ex) {
             Logger.getLogger(DBConnection.class.getName()).log(Level.SEVERE, null, ex);
         }
 }
-    
-
 }
-
