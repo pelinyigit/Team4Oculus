145f7afa2b768eb69f633e6adec75828b6c308b8
Ata <laythese@gmail.com>
2018-12-23 12:48:35 +0300

    Update App.java
    main class for embedded database

--- App.java
+++ App.java
@@ -1,35 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package connectsqlit;
+package softwarepulse.app;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class App {
-    
-/**
- * 
- * @param args 
- */
-public static void main(String[] args) 
-{
-      ConnectSqlit test = new ConnectSqlit();
-      ResultSet rs;
-       
-    try {
-        rs = test.displayUsers();
-        while(rs.next())
-{
-  System.out.println(rs.getString("fName") + " " + rs.getString("lname"));
-}
 
-    } catch (SQLException | ClassNotFoundException ex) {
-        Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-    } 
-}
+	public static void main(String[] args) {
+		
+		ConnectSqlit test = new ConnectSqlit();
+		ResultSet rs;
+		
+		try {
+			  rs = test.displayUsers();
+			  
+			  while (rs.next()) {
+				     System.out.println(rs.getString("fname") + " " + rs.getString("lname"));
+				  }
+			  
+		  } catch (ClassNotFoundException | SQLException e) {
+		  }
+
+	}
+
 }
