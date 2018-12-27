10f89801490323f3b6cce78150f4aa63549b0ff9
Ata <laythese@gmail.com>
2018-12-27 04:17:11 +0300

    Update OculusFinal.java

--- OculusFinal/src/oculusfinal/OculusFinal.java
+++ OculusFinal/src/oculusfinal/OculusFinal.java
@@ -11,7 +11,7 @@ public class OculusFinal
         ConnectDB db = new ConnectDB();
         
         db.getConnection();
-/*
+
         db.createtable("rap");
         db.createtable("Vosvos");
         db.createtable("Graphicscards");
@@ -19,12 +19,17 @@ public class OculusFinal
         
         db.renametable("vosvos", "Cars");
         db.renametable("rap", "Musics");
-*/   
+  
         db.DisplayAllTables();
         
         System.out.println(db.DisplayAllTablesReturns());
         System.out.println(db.getTotalColumn());
         System.out.println(db.getTotalRow()); 
+        System.out.println(db.getAllColumns(2));
+        db.addAttributeInt("books", "deneme");
+        db.createItem("books","deneme" );
+        System.out.println(db. getAllAttributes("Books"));
+
         
 /* 
         db.Terminal("");
